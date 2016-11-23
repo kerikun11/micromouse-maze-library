a49afef2a51b428d72701a22ea0575c8e844f66e
kerikun11 <kerikun11+github@gmail.com>
2016-11-23 23:12:38 +0900

    歩数マップを実装

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <queue>
 
 #define MAZE_SIZE	5
 
@@ -33,14 +34,48 @@ class MazeWall {
 			else byte &= ~(0x01<<index);
 		}
 		inline bool isDoneAll() const { return (byte | 0x0f) == 0xff; }
+		inline int nWall() const {
+			uint8_t b = byte & 0x0f;
+			b = (b & 0x55) + (b >> 1 & 0x55);
+			b = (b & 0x33) + (b >> 2 & 0x33);
+			b = (b & 0x0f) + (b >> 4 & 0x0f);
+			return b;
+		}
+		inline int nDoneWall() const {
+			uint8_t b = (byte & 0xf0) >> 4;
+			b = (b & 0x55) + (b >> 1 & 0x55);
+			b = (b & 0x33) + (b >> 2 & 0x33);
+			b = (b & 0x0f) + (b >> 4 & 0x0f);
+			return b;
+		}
 };
 
-class MazePosition {
+class MazeVector {
 	public:
-		MazePosition(uint8_t x=0, uint8_t y=0, uint8_t dir=0) : x(x), y(y), dir(dir) {}
-		uint8_t x;
-		uint8_t y;
-		uint8_t dir;
+		MazeVector(int8_t x=0, int8_t y=0) : x(x), y(y) {}
+		int8_t x;
+		int8_t y;
+		inline MazeVector operator+(const MazeVector &obj) const { return MazeVector(x+obj.x, y+obj.y); }
+		inline MazeVector operator-(const MazeVector &obj) const { return MazeVector(x-obj.x, y-obj.y); }
+		inline void operator+=(const MazeVector &obj) { x+=obj.x; y+=obj.y; }
+		inline void operator-=(const MazeVector &obj) { x-=obj.x; y-=obj.y; }
+		inline const MazeVector& operator=(const MazeVector &obj) { x=obj.x; y=obj.y; return *this; }
+		inline bool operator==(const MazeVector &obj) const { return x == obj.x && y == obj.y; }
+		inline bool operator!=(const MazeVector &obj) const { return x != obj.x || y != obj.y; }
+		static const MazeVector dirVector(int dir){
+			switch(dir){
+				case 0:
+					return MazeVector(0,1);
+				case 1:
+					return MazeVector(1,0);
+				case 2:
+					return MazeVector(0,-1);
+				case 3:
+					return MazeVector(-1,0);
+				default:
+					return MazeVector();
+			}
+		}
 };
 
 class Maze {
@@ -51,7 +86,7 @@ class Maze {
 		const Maze& operator=(const Maze &obj){
 			for (int i=0;i<MAZE_SIZE;i++) {
 				for (int j=0;j<MAZE_SIZE;j++) {
-					_wall[i][j] = obj._wall[i][j];
+					wall[i][j] = obj.wall[i][j];
 					stepMap[i][j] = obj.stepMap[i][j];
 				}
 			}
@@ -59,61 +94,63 @@ class Maze {
 		}
 		void reset(){
 			for(int i=0; i<MAZE_SIZE; i++){
-				_wall[i][MAZE_SIZE-1].bits.East=1;
-				_wall[i][MAZE_SIZE-1].bits.DoneEast=1;
-				_wall[i][0].bits.West=1;
-				_wall[i][0].bits.DoneWest=1;
-				_wall[MAZE_SIZE-1][i].bits.North=1;
-				_wall[MAZE_SIZE-1][i].bits.DoneNorth=1;
-				_wall[0][i].bits.South=1;
-				_wall[0][i].bits.DoneSouth=1;
+				wall[i][MAZE_SIZE-1].bits.East=1;
+				wall[i][MAZE_SIZE-1].bits.DoneEast=1;
+				wall[i][0].bits.West=1;
+				wall[i][0].bits.DoneWest=1;
+				wall[MAZE_SIZE-1][i].bits.North=1;
+				wall[MAZE_SIZE-1][i].bits.DoneNorth=1;
+				wall[0][i].bits.South=1;
+				wall[0][i].bits.DoneSouth=1;
 			}
 		}
-		void update(const MazePosition &pos, const MazeWall &wall){
-			_wall[pos.y][pos.x] = wall;
-			if(pos.x-1>=0){
-				_wall[pos.y][pos.x-1].bits.East = wall.bits.West;
-				_wall[pos.y][pos.x-1].bits.DoneEast = wall.bits.DoneWest;
+		void update(const MazeVector &vec, const MazeWall &w){
+			wall[vec.y][vec.x] = w;
+			if(vec.x-1>=0){
+				wall[vec.y][vec.x-1].bits.East = w.bits.West;
+				wall[vec.y][vec.x-1].bits.DoneEast = w.bits.DoneWest;
 			}
-			if(pos.y-1>=0){
-				_wall[pos.y-1][pos.x].bits.North = wall.bits.South;
-				_wall[pos.y-1][pos.x].bits.DoneNorth = wall.bits.DoneSouth;
+			if(vec.y-1>=0){
+				wall[vec.y-1][vec.x].bits.North = w.bits.South;
+				wall[vec.y-1][vec.x].bits.DoneNorth = w.bits.DoneSouth;
 			}
-			if(pos.x+1<MAZE_SIZE){
-				_wall[pos.y][pos.x+1].bits.West = wall.bits.East;
-				_wall[pos.y][pos.x+1].bits.DoneWest = wall.bits.DoneEast;
+			if(vec.x+1<MAZE_SIZE){
+				wall[vec.y][vec.x+1].bits.West = w.bits.East;
+				wall[vec.y][vec.x+1].bits.DoneWest = w.bits.DoneEast;
 			}
-			if(pos.y+1>=0){
-				_wall[pos.y+1][pos.x].bits.South = wall.bits.North;
-				_wall[pos.y+1][pos.x].bits.DoneSouth = wall.bits.DoneNorth;
+			if(vec.y+1<MAZE_SIZE){
+				wall[vec.y+1][vec.x].bits.South = w.bits.North;
+				wall[vec.y+1][vec.x].bits.DoneSouth = w.bits.DoneNorth;
 			}
 		}
-		void update(const MazePosition &pos, bool left, bool flont, bool right, bool back = false){
-			MazeWall wall;
-			wall.update((pos.dir+0)&0x3, flont ? 1 : 0);
-			wall.update((pos.dir+1)&0x3, right ? 1 : 0);
-			wall.update((pos.dir+2)&0x3, back ? 1 : 0);
-			wall.update((pos.dir+3)&0x3, left ? 1 : 0);
-			wall |= 0xF0;
-			update(pos, wall);
+		void update(const MazeVector &vec, bool left, bool front, bool right, bool back = false){
+			/*
+			   MazeWall w;
+			   w.update((vec.dir+0)&0x3, front ? 1 : 0);
+			   w.update((vec.dir+1)&0x3, right ? 1 : 0);
+			   w.update((vec.dir+2)&0x3, back ? 1 : 0);
+			   w.update((vec.dir+3)&0x3, left ? 1 : 0);
+			   w |= 0xF0;
+			   update(vec, w);
+			   */
 		}
 		void printWall(uint8_t nums[MAZE_SIZE][MAZE_SIZE] = NULL){
 			for(int y=MAZE_SIZE-1; y>=0; y--){
 				for(int x=0; x<MAZE_SIZE; x++){
-					printf("+%s+",_wall[y][x].bits.North?"---":"   ");
+					printf("+%s+",wall[y][x].bits.North?"---":"   ");
 				}
 				printf("\n");
 
 				for(uint8_t x=0; x<MAZE_SIZE; x++){
-					printf("%s", _wall[y][x].bits.West?"|":" ");
+					printf("%s", wall[y][x].bits.West?"|":" ");
 					if(nums==NULL) printf("   ");
 					else printf("%3d", nums[y][x]);
-					printf("%s", _wall[y][x].bits.East?"|":" ");
+					printf("%s", wall[y][x].bits.East?"|":" ");
 				}
 				printf("\n");
 
 				for(int x=0; x<MAZE_SIZE; x++){
-					printf("+%s+",_wall[y][x].bits.South?"---":"   ");
+					printf("+%s+",wall[y][x].bits.South?"---":"   ");
 				}
 				printf("\n");
 			}
@@ -124,40 +161,47 @@ class Maze {
 				for(int j=0;j<MAZE_SIZE;j++){
 					char ch = asciiData[MAZE_SIZE-1-i][j];
 					if(('0' <= ch && ch <= '9') || ('a' <= ch && ch <= 'f')){
-						uint8_t wall_bin;
-						if('0' <= ch && ch <= '9') wall_bin = ch - '0';
-						else wall_bin = ch - 'a' + 10;
-						_wall[i][j].byte = wall_bin | 0xf0;
+						uint8_t w;
+						if('0' <= ch && ch <= '9') w = ch - '0';
+						else w = ch - 'a' + 10;
+						wall[i][j].byte = w | 0xf0;
 					}
 				}
 			}
 		}
-		uint8_t getWall(const MazePosition &pos){return _wall[pos.y][pos.x];}
-		uint8_t getWall(const uint8_t x, const uint8_t y){return _wall[y][x];}
-		uint8_t getStepMap(const MazePosition &pos){return stepMap[pos.y][pos.x];}
+		uint8_t getWall(const MazeVector &vec){return wall[vec.y][vec.x];}
+		uint8_t getWall(const uint8_t x, const uint8_t y){return wall[y][x];}
+		uint8_t getStepMap(const MazeVector &vec){return stepMap[vec.y][vec.x];}
 		uint8_t getStepMap(const uint8_t x, const uint8_t y){return stepMap[y][x];}
-		void updateStepMap(const MazePosition &dist){
+		void updateStepMap(const MazeVector &dist, bool onlyUseFoundWall = false){
 			for (int i=0;i<MAZE_SIZE;i++) {
 				for (int j=0;j<MAZE_SIZE;j++) {
 					stepMap[i][j] = 255;
 				}
 			}
 			stepMap[dist.y][dist.x]=0;
-			MazePosition cur = dist;
-			bool changed = true;
-			for(int i=0; changed; i++){
-				changed = false;
-				MazeWall wall = getWall(cur);
-				for(int j=0; j<4; j++){
-					if(!wall[j]){
-
+			std::queue<MazeVector> q;
+			q.push(dist);
+			while(q.size()){
+				const MazeVector cur = q.front();
+				q.pop();
+				MazeWall curWall = wall[cur.y][cur.x];
+				for(int i=0; i<4; i++){
+					const MazeVector scan = cur + MazeVector::dirVector(i);
+					const uint8_t curStep = stepMap[cur.y][cur.x];
+					if (!curWall[i] && stepMap[scan.y][scan.x] > curStep +1) {
+						if (onlyUseFoundWall && !curWall[i+4]) continue;
+						stepMap[scan.y][scan.x] = curStep +1;
+						if (wall[scan.y][scan.x].nWall() != 3) {
+							q.push(scan);
+						}
 					}
 				}
 			}
 			printWall(stepMap);
 		}
 	private:
-		MazeWall _wall[MAZE_SIZE][MAZE_SIZE];	// _wall[y][x]
+		MazeWall wall[MAZE_SIZE][MAZE_SIZE];	// wall[y][x]
 		uint8_t stepMap[MAZE_SIZE][MAZE_SIZE];
 };
 
@@ -168,9 +212,9 @@ class MazeAgent {
 		}
 	private:
 		Maze *maze;
-		MazePosition current;
-		MazePosition virtualDist;
-		MazePosition destination;
+		MazeVector current;
+		MazeVector virtualDist;
+		MazeVector destination;
 		enum State{
 			IDLE,
 			SEARCHING_FOR_GOAL,
@@ -195,7 +239,7 @@ int main(){
 	maze_target.printWall();
 	maze_backup = maze;
 
-	maze.updateStepMap(MazePosition(2,2,0));
+	maze.updateStepMap(MazeVector(2,2));
 	return 0;
 }
 
