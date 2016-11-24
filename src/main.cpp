1d6da876fc186a59c331a70c335ed6805e9e94c0
kerikun11 <kerikun11+github@gmail.com>
2016-11-24 19:45:03 +0900

    ゴールに到達

--- src/main.cpp
+++ src/main.cpp
@@ -3,158 +3,61 @@
 
 #define MAZE_SIZE	5
 
-class MazeWall {
+#define NORTH		0x01	// North Wall
+#define EAST		0x02
+#define SOUTH		0x04
+#define WEST		0x08
+#define D_NORTH		0x10	// Done North Wall
+#define D_EAST		0x20
+#define D_SOUTH		0x40
+#define D_WEST		0x80
+#define F_NORTH		0x11	// Found North Wall
+#define F_EAST		0x22
+#define F_SOUTH		0x44
+#define F_WEST		0x88
+#define N_NORTH		0x10	// Not Found North Wall
+#define N_EAST		0x20
+#define N_SOUTH		0x40
+#define N_WEST		0x80
+
+struct IndexVec{
 	public:
-		union{
-			uint8_t byte;
-			struct{
-				uint8_t North:1;
-				uint8_t East:1;
-				uint8_t South:1;
-				uint8_t West:1;
-				uint8_t DoneNorth:1;
-				uint8_t DoneEast:1;
-				uint8_t DoneSouth:1;
-				uint8_t DoneWest:1;
-			}bits;
-		};
-		MazeWall(uint8_t byte=0) : byte(byte) {}
-
-		inline operator uint8_t() const { return byte; }
-		inline uint8_t operator|(uint8_t value) const { return byte | value; }
-		inline uint8_t operator&(uint8_t value) const { return byte & value; }
-		inline uint8_t operator=(uint8_t value) { return byte = value; }
-		inline uint8_t operator|=(uint8_t value) { return byte |= value; }
-		inline uint8_t operator&=(uint8_t value) { return byte &= value; }
-		inline uint8_t operator=(MazeWall &obj) { return byte = obj.byte; }
-		inline uint8_t operator[](uint8_t index) const {return (byte & (0x01<<index)) ? 1:0; }
-
-		inline void update(uint8_t index, uint8_t value){
-			if(value) byte |= (0x01<<index);
-			else byte &= ~(0x01<<index);
-		}
-		inline bool isDoneAll() const { return (byte | 0x0f) == 0xff; }
-		inline int nWall() const {
-			uint8_t b = byte & 0x0f;
-			b = (b & 0x55) + (b >> 1 & 0x55);
-			b = (b & 0x33) + (b >> 2 & 0x33);
-			b = (b & 0x0f) + (b >> 4 & 0x0f);
-			return b;
-		}
-		inline int nDoneWall() const {
-			uint8_t b = (byte & 0xf0) >> 4;
-			b = (b & 0x55) + (b >> 1 & 0x55);
-			b = (b & 0x33) + (b >> 2 & 0x33);
-			b = (b & 0x0f) + (b >> 4 & 0x0f);
-			return b;
-		}
-};
-
-class MazeVector {
-	public:
-		MazeVector(int8_t x=0, int8_t y=0) : x(x), y(y) {}
 		int8_t x;
 		int8_t y;
-		inline MazeVector operator+(const MazeVector &obj) const { return MazeVector(x+obj.x, y+obj.y); }
-		inline MazeVector operator-(const MazeVector &obj) const { return MazeVector(x-obj.x, y-obj.y); }
-		inline void operator+=(const MazeVector &obj) { x+=obj.x; y+=obj.y; }
-		inline void operator-=(const MazeVector &obj) { x-=obj.x; y-=obj.y; }
-		inline const MazeVector& operator=(const MazeVector &obj) { x=obj.x; y=obj.y; return *this; }
-		inline bool operator==(const MazeVector &obj) const { return x == obj.x && y == obj.y; }
-		inline bool operator!=(const MazeVector &obj) const { return x != obj.x || y != obj.y; }
-		static const MazeVector dirVector(int dir){
-			switch(dir){
-				case 0:
-					return MazeVector(0,1);
-				case 1:
-					return MazeVector(1,0);
-				case 2:
-					return MazeVector(0,-1);
-				case 3:
-					return MazeVector(-1,0);
-				default:
-					return MazeVector();
-			}
-		}
+
+		IndexVec(int8_t x=0, int8_t y=0):x(x), y(y) {}
+		inline IndexVec operator+(const IndexVec &obj) const { return IndexVec(x+obj.x, y+obj.y); }
+		inline IndexVec operator-(const IndexVec &obj) const { return IndexVec(x-obj.x, y-obj.y); }
+		inline void operator+=(const IndexVec &obj) { x+=obj.x; y+=obj.y; }
+		inline void operator-=(const IndexVec &obj) { x-=obj.x; y-=obj.y; }
+		inline const IndexVec& operator=(const IndexVec &obj) { x=obj.x; y=obj.y; return *this; }
+		inline bool operator==(const IndexVec &obj) const { return x == obj.x && y == obj.y; }
+		inline bool operator!=(const IndexVec &obj) const { return x != obj.x || y != obj.y; }
 };
 
-class Maze {
+class Maze{
 	public:
 		Maze(){
 			reset();
 		}
-		const Maze& operator=(const Maze &obj){
-			for (int i=0;i<MAZE_SIZE;i++) {
-				for (int j=0;j<MAZE_SIZE;j++) {
-					wall[i][j] = obj.wall[i][j];
-					stepMap[i][j] = obj.stepMap[i][j];
-				}
-			}
-			return *this;
-		}
 		void reset(){
+			for(uint8_t y=0; y<MAZE_SIZE; y++)
+				for(uint8_t x=0; x<MAZE_SIZE; x++)
+					wall[y][x]=0x00;
 			for(int i=0; i<MAZE_SIZE; i++){
-				wall[i][MAZE_SIZE-1].bits.East=1;
-				wall[i][MAZE_SIZE-1].bits.DoneEast=1;
-				wall[i][0].bits.West=1;
-				wall[i][0].bits.DoneWest=1;
-				wall[MAZE_SIZE-1][i].bits.North=1;
-				wall[MAZE_SIZE-1][i].bits.DoneNorth=1;
-				wall[0][i].bits.South=1;
-				wall[0][i].bits.DoneSouth=1;
+				wall[i][MAZE_SIZE-1] |= EAST|D_EAST;
+				wall[i][0] |= WEST|D_WEST;
+				wall[MAZE_SIZE-1][i] |= NORTH|D_NORTH;
+				wall[0][i] |= SOUTH|D_SOUTH;
 			}
 		}
-		void update(const MazeVector &vec, const MazeWall &w){
-			wall[vec.y][vec.x] = w;
-			if(vec.x-1>=0){
-				wall[vec.y][vec.x-1].bits.East = w.bits.West;
-				wall[vec.y][vec.x-1].bits.DoneEast = w.bits.DoneWest;
-			}
-			if(vec.y-1>=0){
-				wall[vec.y-1][vec.x].bits.North = w.bits.South;
-				wall[vec.y-1][vec.x].bits.DoneNorth = w.bits.DoneSouth;
-			}
-			if(vec.x+1<MAZE_SIZE){
-				wall[vec.y][vec.x+1].bits.West = w.bits.East;
-				wall[vec.y][vec.x+1].bits.DoneWest = w.bits.DoneEast;
-			}
-			if(vec.y+1<MAZE_SIZE){
-				wall[vec.y+1][vec.x].bits.South = w.bits.North;
-				wall[vec.y+1][vec.x].bits.DoneSouth = w.bits.DoneNorth;
-			}
-		}
-		void update(const MazeVector &vec, bool left, bool front, bool right, bool back = false){
-			/*
-			   MazeWall w;
-			   w.update((vec.dir+0)&0x3, front ? 1 : 0);
-			   w.update((vec.dir+1)&0x3, right ? 1 : 0);
-			   w.update((vec.dir+2)&0x3, back ? 1 : 0);
-			   w.update((vec.dir+3)&0x3, left ? 1 : 0);
-			   w |= 0xF0;
-			   update(vec, w);
-			   */
-		}
-		void printWall(uint8_t nums[MAZE_SIZE][MAZE_SIZE] = NULL){
-			for(int y=MAZE_SIZE-1; y>=0; y--){
-				for(int x=0; x<MAZE_SIZE; x++){
-					printf("+%s+",wall[y][x].bits.North?"---":"   ");
-				}
-				printf("\n");
-
+		inline Maze& operator=(const Maze& obj){
+			for(uint8_t y=0; y<MAZE_SIZE; y++)
 				for(uint8_t x=0; x<MAZE_SIZE; x++){
-					printf("%s", wall[y][x].bits.West?"|":" ");
-					if(nums==NULL) printf("   ");
-					else printf("%3d", nums[y][x]);
-					printf("%s", wall[y][x].bits.East?"|":" ");
+					wall[y][x] = obj.wall[y][x];
+					stepMap[y][x] = obj.stepMap[y][x];
 				}
-				printf("\n");
-
-				for(int x=0; x<MAZE_SIZE; x++){
-					printf("+%s+",wall[y][x].bits.South?"---":"   ");
-				}
-				printf("\n");
-			}
-			printf("\n");
+			return *this;
 		}
 		void loadFromArray(const char asciiData[MAZE_SIZE+1][MAZE_SIZE+1]){
 			for(int i=0;i<MAZE_SIZE;i++){
@@ -164,69 +67,150 @@ class Maze {
 						uint8_t w;
 						if('0' <= ch && ch <= '9') w = ch - '0';
 						else w = ch - 'a' + 10;
-						wall[i][j].byte = w | 0xf0;
+						wall[i][j] = w|0xf0;
 					}
 				}
 			}
 		}
-		uint8_t getWall(const MazeVector &vec){return wall[vec.y][vec.x];}
-		uint8_t getWall(const uint8_t x, const uint8_t y){return wall[y][x];}
-		uint8_t getStepMap(const MazeVector &vec){return stepMap[vec.y][vec.x];}
-		uint8_t getStepMap(const uint8_t x, const uint8_t y){return stepMap[y][x];}
-		void updateStepMap(const MazeVector &dist, bool onlyUseFoundWall = false){
-			for (int i=0;i<MAZE_SIZE;i++) {
-				for (int j=0;j<MAZE_SIZE;j++) {
-					stepMap[i][j] = 255;
+		inline uint8_t getWall(const uint8_t x, const uint8_t y) const {return wall[y][x];}
+		inline uint8_t getStepMap(const uint8_t x, const uint8_t y) const {return stepMap[y][x];}
+		void updateWall(const uint8_t x, const uint8_t y, const uint8_t w){
+			wall[y][x] |= w;
+			if(x != 0          ) wall[y][x-1] |= (w&F_WEST )>>2; //< Not Westernmost
+			if(x != MAZE_SIZE-1) wall[y][x+1] |= (w&F_EAST )<<2; //< Not Easternmost
+			if(y != 0          ) wall[y-1][x] |= (w&F_SOUTH)>>2; //< Not Southernmost
+			if(y != MAZE_SIZE-1) wall[y+1][x] |= (w&F_NORTH)<<2; //< Not Northernmost
+		}
+		inline void updateWall(const IndexVec& v, const uint8_t w) { return updateWall(v.x, v.y, w);}
+		void updateWall(const uint8_t x, const uint8_t y, const uint8_t dir, const bool left, const bool front, const bool right){
+			uint8_t w=0x00;
+			w |= (left  ? F_NORTH : N_NORTH) << ((dir+3)&0x03);
+			w |= (front ? F_NORTH : N_NORTH) << ((dir+0)&0x03);
+			w |= (right ? F_NORTH : N_NORTH) << ((dir+1)&0x03);
+			updateWall(x,y,w);
+		}
+		void printWall(const IndexVec& v){
+			for(int16_t y=MAZE_SIZE-1; y>=0; y--){
+				for(uint8_t x=0; x<MAZE_SIZE; x++) printf("+%s+", wall[y][x]&NORTH ? "---" : "   ");
+				printf("\n");
+				for(uint8_t x=0; x<MAZE_SIZE; x++){
+					printf("%s", wall[y][x]&WEST ? "|" : " ");
+					if(x==v.x&&y==v.y) printf(" * ");
+					else printf("   ");
+					printf("%s", wall[y][x]&EAST ? "|" : " ");
 				}
+				printf("\n");
+				for(uint8_t x=0; x<MAZE_SIZE; x++) printf("+%s+", wall[y][x]&SOUTH ? "---" : "   ");
+				printf("\n");
 			}
-			stepMap[dist.y][dist.x]=0;
-			std::queue<MazeVector> q;
-			q.push(dist);
+			printf("\n");
+		}
+		void printWall(uint8_t nums[MAZE_SIZE][MAZE_SIZE] = NULL){
+			for(int16_t y=MAZE_SIZE-1; y>=0; y--){
+				for(uint8_t x=0; x<MAZE_SIZE; x++) printf("+%s+", wall[y][x]&NORTH ? "---" : "   ");
+				printf("\n");
+				for(uint8_t x=0; x<MAZE_SIZE; x++){
+					printf("%s", wall[y][x]&WEST ? "|" : " ");
+					if(nums==NULL) printf("   ");
+					else printf("%3d", nums[y][x]);
+					printf("%s", wall[y][x]&EAST ? "|" : " ");
+				}
+				printf("\n");
+				for(uint8_t x=0; x<MAZE_SIZE; x++) printf("+%s+", wall[y][x]&SOUTH ? "---" : "   ");
+				printf("\n");
+			}
+			printf("\n");
+		}
+		void updateStepMap(const IndexVec& dest){
+			for(uint8_t y=0; y<MAZE_SIZE; y++)
+				for(uint8_t x=0; x<MAZE_SIZE; x++)
+					stepMap[y][x] = 0xff;
+			stepMap[dest.y][dest.x] = 0;
+			std::queue<IndexVec> q;
+			q.push(dest);
 			while(q.size()){
-				const MazeVector cur = q.front();
-				q.pop();
-				MazeWall curWall = wall[cur.y][cur.x];
-				for(int i=0; i<4; i++){
-					const MazeVector scan = cur + MazeVector::dirVector(i);
-					const uint8_t curStep = stepMap[cur.y][cur.x];
-					if (!curWall[i] && stepMap[scan.y][scan.x] > curStep +1) {
-						if (onlyUseFoundWall && !curWall[i+4]) continue;
-						stepMap[scan.y][scan.x] = curStep +1;
-						if (wall[scan.y][scan.x].nWall() != 3) {
-							q.push(scan);
-						}
-					}
+				IndexVec focus = q.front(); q.pop();
+				uint8_t focus_step = stepMap[focus.y][focus.x];
+				uint8_t focus_wall = wall[focus.y][focus.x];
+				if(!(focus_wall&NORTH) && stepMap[focus.y+1][focus.x]>focus_step+1){
+					stepMap[focus.y+1][focus.x] = focus_step+1;
+					q.push(IndexVec(focus.x, focus.y+1));
+				}
+				if(!(focus_wall&SOUTH) && stepMap[focus.y-1][focus.x]>focus_step+1){
+					stepMap[focus.y-1][focus.x] = focus_step+1;
+					q.push(IndexVec(focus.x, focus.y-1));
+				}
+				if(!(focus_wall&EAST) && stepMap[focus.y][focus.x+1]>focus_step+1){
+					stepMap[focus.y][focus.x+1] = focus_step+1;
+					q.push(IndexVec(focus.x+1, focus.y));
+				}
+				if(!(focus_wall&WEST) && stepMap[focus.y][focus.x-1]>focus_step+1){
+					stepMap[focus.y][focus.x-1] = focus_step+1;
+					q.push(IndexVec(focus.x-1, focus.y));
 				}
 			}
 			printWall(stepMap);
 		}
 	private:
-		MazeWall wall[MAZE_SIZE][MAZE_SIZE];	// wall[y][x]
+		uint8_t wall[MAZE_SIZE][MAZE_SIZE];
 		uint8_t stepMap[MAZE_SIZE][MAZE_SIZE];
 };
 
-class MazeAgent {
+class MazeAgent{
 	public:
-		MazeAgent(Maze *maze):maze(maze){
-			state = IDLE;
+		MazeAgent(Maze* maze, const IndexVec& dest):maze(maze), dest(dest){
+			state = SEARCHING_FOR_GOAL;
 		}
-	private:
-		Maze *maze;
-		MazeVector current;
-		MazeVector virtualDist;
-		MazeVector destination;
 		enum State{
-			IDLE,
 			SEARCHING_FOR_GOAL,
 			SEARCHING_EXTRA,
 			GOING_TO_START,
 			FINISHED,
-		}state;
+		};
+		enum Action{
+			GO_STRAIGHT,
+			TURN_LEFT_90,
+			TURN_RIGHT_90,
+			RETURN,
+		};
+		enum State getState(){
+			return state;
+		}
+		IndexVec getNextVector(const IndexVec& cur, uint8_t cur_wall){
+			if(state == SEARCHING_FOR_GOAL){
+				maze->updateWall(cur, cur_wall);
+				maze->updateStepMap(dest);
+				if(cur == dest){
+					printf("Finished!\n");
+					state = FINISHED;
+				}else{
+					uint8_t cur_step = maze->getStepMap(cur.x, cur.y);
+					if(!(cur_wall&NORTH) && maze->getStepMap(cur.x, cur.y+1)==cur_step-1){
+						return cur+IndexVec(0, 1);
+					}
+					if(!(cur_wall&SOUTH) && maze->getStepMap(cur.x, cur.y-1)==cur_step-1){
+						return cur+IndexVec(0, -1);
+					}
+					if(!(cur_wall&EAST) && maze->getStepMap(cur.x+1, cur.y)==cur_step-1){
+						return cur+IndexVec(1, 0);
+					}
+					if(!(cur_wall&WEST) && maze->getStepMap(cur.x-1, cur.y)==cur_step-1){
+						return cur+IndexVec(-1, 0);
+					}
+				}
+			}
+		}
+	private:
+		Maze *maze;
+		enum State state;
+		IndexVec dest;
 };
 
 int main(){
 	Maze maze, maze_backup;
-	MazeAgent agent(&maze);
+	IndexVec dest(2,2);
+	MazeAgent agent(&maze, dest);
+
 	const char mazeData_55test[5+1][5+1] = {
 		{"91513"},
 		{"aad6a"},
@@ -237,9 +221,26 @@ int main(){
 	Maze maze_target;
 	maze_target.loadFromArray(mazeData_55test);
 	maze_target.printWall();
-	maze_backup = maze;
 
-	maze.updateStepMap(MazeVector(2,2));
+	IndexVec cur;
+	while(agent.getState() != MazeAgent::FINISHED){
+		IndexVec next = agent.getNextVector(cur, maze_target.getWall(cur.x, cur.y));
+		cur = next;
+		maze.printWall(cur);
+	}
+	/*
+	   maze.updateWall(0,0,0,true,false,true);
+	   maze.updateWall(0,1,0,true,true,false);
+	   maze.updateWall(1,1,1,true,false,true);
+	   maze.updateWall(2,1,1,true,true,false);
+	   maze.updateWall(2,0,2,false,true,false);
+	   maze.updateWall(3,0,1,false,true,true);
+	   maze.updateWall(3,1,0,true,false,true);
+	   maze.updateWall(3,2,0,true,false,true);
+	   maze.updateWall(3,3,0,true,true,false);
+	   maze.updateWall(4,3,1,false,true,false);
+	   maze.updateWall(4,4,0,false,true,true);
+	   */
+
 	return 0;
 }
-
