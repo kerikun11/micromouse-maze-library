35f8eb657a210c77c02132facadaa5984a857402
kerikun11 <kerikun11+github@gmail.com>
2017-05-02 15:01:27 +0900

    Dirクラスを作成
    方向をしっかり管理

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,13 @@
 #include <cstdio>
 #include <queue>
 #include <vector>
+#include <array>
 #include <cstdint>
+#include <algorithm>
 #include <unistd.h>
 
-#define MAZE_SIZE		8
+#define MAZE_SIZE		16
 #define MAZE_STEP_MAX	999
-
-#define EAST		0x01	// Wall
-#define NORTH		0x02
-#define WEST		0x04
-#define SOUTH		0x08
-#define D_EAST		0x10	// Done Wall
-#define D_NOTTH		0x20
-#define D_WEST		0x40
-#define D_SOUTH		0x80
-#define F_EAST		0x11	// Found Wall
-#define F_NORTH		0x22
-#define F_WEST		0x44
-#define F_SOUTH		0x88
-#define N_EAST		0x10	// Not Found Wall
-#define N_NORTH		0x20
-#define N_WEST		0x40
-#define N_SOUTH		0x80
 
 #define C_RED     "\x1b[31m"
 #define C_GREEN   "\x1b[32m"
@@ -34,6 +19,33 @@
 
 typedef int8_t dir_t;
 typedef uint16_t step_t;
+
+class Dir{
+	public:
+		enum AbsoluteDir: uint8_t { East, North, West, South, AbsMax };
+		Dir(const enum AbsoluteDir d = East):d(d){}
+		Dir(const uint8_t d):d(AbsoluteDir(d&3)){}
+
+		operator uint8_t() const { return d; }
+		const Dir operator=(const Dir& obj) { this->d = obj.d; return *this; }
+
+		const std::array<Dir, 4> ordered() const {
+			std::array<Dir, 4> order{Forward(), Left(), Right(), Back()};
+			return order;
+		}
+
+		const Dir Forward() const { return Dir(d); }
+		const Dir Left() const { return Dir(d+1); }
+		const Dir Right() const { return Dir(d+3); }
+		const Dir Back() const { return Dir(d+2); }
+
+		static const std::array<Dir, 4>& All(){
+			static const std::array<Dir, 4> all = {East, North, West, South};
+			return all;
+		}
+	private:
+		enum AbsoluteDir d;
+};
 
 union Wall{
 	Wall(uint8_t value = 0) : flags(value) {}
@@ -55,11 +67,12 @@ union Wall{
 	inline uint8_t operator&=(const Wall& obj) { flags&=obj.flags; return flags; }
 
 	inline void reset(){ flags=0; }
-	inline int8_t nWall() const { return e+n+w+s; }
-	inline int8_t nDone() const { return E+N+W+S; }
+	//inline int8_t nWall() const { return e+n+w+s; }
+	inline int8_t nWall() const { return __builtin_popcount(0x0f&flags); }
+	//inline int8_t nDone() const { return E+N+W+S; }
+	inline int8_t nDone() const { return __builtin_popcount(0xf0&flags);; }
 
-	inline void updateOne(dir_t dir, const bool b) {
-		dir &= 3;
+	inline void updateOne(const Dir &dir, const bool b) {
 		if(b) flags |=   1<<dir;
 		else  flags &= ~(1<<dir);
 		flags |= 1<<(dir+4);
@@ -67,8 +80,7 @@ union Wall{
 	inline void updateAll(const uint8_t wall) {
 		flags = 0xF0 | (wall&0x0F);
 	}
-	inline uint8_t rotate(dir_t dir) const {
-		dir &= 3;
+	inline uint8_t rotate(const Dir &dir) const {
 		return ((flags<<dir)|(flags>>(4-dir)))&0x0F;
 	}
 };
@@ -77,19 +89,20 @@ struct Vector{
 	Vector(int8_t x=0, int8_t y=0) : x(x), y(y) {}
 	int8_t x,y;
 
-	inline const Vector& operator=(const Vector& obj) { x=obj.x; y=obj.y; return *this; }
-	inline const Vector& operator+=(const Vector& obj) { x+=obj.x; y+=obj.y; return *this; }
-	inline const Vector& operator-=(const Vector& obj) { x-=obj.x; y-=obj.y; return *this; }
-	inline const Vector operator+(const Vector& obj) const { return Vector(x+obj.x, y+obj.y); }
-	inline const Vector operator-(const Vector& obj) const { return Vector(x-obj.x, y-obj.y); }
-	inline const bool operator==(const Vector& obj) const { return x==obj.x && y==obj.y; }
-	inline const bool operator!=(const Vector& obj) const { return x!=obj.x || y!=obj.y; }
-	inline const Vector next(const dir_t dir) const {
-		switch(dir&0x3){
-			case 0: return Vector(x+1, y);
-			case 1: return Vector(x, y+1);
-			case 2: return Vector(x-1, y);
-			case 3: return Vector(x, y-1);
+	const Vector& operator=(const Vector& obj) { x=obj.x; y=obj.y; return *this; }
+	const Vector& operator+=(const Vector& obj) { x+=obj.x; y+=obj.y; return *this; }
+	const Vector& operator-=(const Vector& obj) { x-=obj.x; y-=obj.y; return *this; }
+	const Vector operator+(const Vector& obj) const { return Vector(x+obj.x, y+obj.y); }
+	const Vector operator-(const Vector& obj) const { return Vector(x-obj.x, y-obj.y); }
+	const bool operator==(const Vector& obj) const { return x==obj.x && y==obj.y; }
+	const bool operator!=(const Vector& obj) const { return x!=obj.x || y!=obj.y; }
+
+	const Vector next(const Dir &dir) const {
+		switch(dir){
+			case Dir::East: return Vector(x+1, y);
+			case Dir::North: return Vector(x, y+1);
+			case Dir::West: return Vector(x-1, y);
+			case Dir::South: return Vector(x, y-1);
 		}
 	}
 };
@@ -126,13 +139,13 @@ class Maze{
 					getWall(x, y).reset();
 			//* set outline
 			for(uint8_t i=0; i<MAZE_SIZE; i++){
-				getWall(i, 0) |= F_SOUTH;
-				getWall(0, i) |= F_WEST;
-				getWall(MAZE_SIZE-1, i) |= F_EAST;
-				getWall(i, MAZE_SIZE-1) |= F_NORTH;
+				getWall(i, 0).updateOne(Dir::South, true);
+				getWall(0, i).updateOne(Dir::West, true);
+				getWall(MAZE_SIZE-1, i).updateOne(Dir::East, true);
+				getWall(i, MAZE_SIZE-1).updateOne(Dir::North, true);
 			}
 			//* start cell
-			updateWall(0,0,F_EAST|N_NORTH|F_WEST|F_SOUTH);
+			updateWall(0,0,0x0b);
 		}
 		inline Wall& getWall(const Vector& v) { return getWall(v.x, v.y); }
 		inline Wall& getWall(const int x, const int y) {
@@ -153,14 +166,12 @@ class Maze{
 		}
 		inline void updateWall(const int x, const int y, const Wall& w){ return updateWall(Vector(x,y),w); }
 		inline void updateWall(const Vector& v, Wall w){
-			if(v.x==0) w.flags |= F_WEST;
-			if(v.y==0) w.flags |= F_SOUTH;
-			if(v.x==MAZE_SIZE-1) w.flags |= F_EAST;
-			if(v.y==MAZE_SIZE-1) w.flags |= F_NORTH;
+			if(v.x==0) w.updateOne(Dir::West, true);
+			if(v.y==0) w.updateOne(Dir::South, true);
+			if(v.x==MAZE_SIZE-1) w.updateOne(Dir::East, true);
+			if(v.y==MAZE_SIZE-1) w.updateOne(Dir::North, true);
 			getWall(v) = w;
-			for(dir_t dir=0; dir<4; dir++){
-				getWall(v.next(dir)).updateOne(dir+2, w[dir]);
-			}
+			for(Dir d: Dir::All()) getWall(v.next(d)).updateOne(2+d, w[d]); //< also update every next wall
 		}
 		inline void printWall(step_t nums[MAZE_SIZE][MAZE_SIZE] = NULL, const Vector v = Vector(-1,-1)){
 #if 0
@@ -217,7 +228,7 @@ class Maze{
 				Vector focus = q.front(); q.pop();
 				step_t focus_step = getStep(focus);
 				Wall focus_wall = getWall(focus);
-				for(dir_t dir=0; dir<4; dir++){
+				for(Dir dir: Dir::All()){
 					Vector next = focus.next(dir);
 					if(!focus_wall[dir] && getStep(next)>focus_step+1){
 						getStep(next) = focus_step+1;
@@ -258,7 +269,7 @@ class MazeAgent{
 			return str[s];
 		}
 
-		void update(const Vector v, const dir_t d, const Wall& w){
+		void update(const Vector v, const Dir &d, const Wall& w){
 			curVec = v;
 			curDir = d;
 			if(state == IDOLE){
@@ -269,45 +280,49 @@ class MazeAgent{
 				maze.updateWall(v, w);
 				maze.updateStepMap(goal);
 				step_t min_step = MAZE_STEP_MAX;
-				for(dir_t dir: {0+d, 1+d, 3+d, 2+d}){
-					dir &= 3;
+				for(Dir dir: d.ordered()){
 					if(maze.getStep(v.next(dir))<min_step && !maze.getWall(v)[dir]){
 						min_step = maze.getStep(v.next(dir));
 						nextDir = dir;
 					}
 				}
 				if(min_step == MAZE_STEP_MAX) state = GOT_LOST;
-				for(auto g: goal){
-					if(v==g) {
-						state = REACHED_GOAL;
-						list = goal;
-					}
+				if(std::find(goal.begin(), goal.end(),v)!=goal.end()){
+					state = REACHED_GOAL;
+					candidates = goal;
+					candidates.erase(std::find(candidates.begin(), candidates.end(), v));
 				}
 			}
 
 			if(state == REACHED_GOAL){
 				maze.updateWall(v, w);
-				maze.updateStepMap(list);
+				maze.updateStepMap(candidates);
 				step_t min_step = MAZE_STEP_MAX;
-				for(int dir: {0+d, 1+d, 3+d}){
+				for(dir_t dir: {0+d, 1+d, 3+d}){
 					dir &= 3;
 					if(maze.getStep(v.next(dir))<min_step && !maze.getWall(v)[dir]){
 						min_step = maze.getStep(v.next(dir));
 						nextDir = dir;
 					}
 				}
-				for(auto it=list.begin();it != list.end()+1; it++){
-					if(v==*it) {
-						list.erase(it);
-						break;
-					}
-				}
-				if(list.empty()){
+				candidates.erase(std::remove(candidates.begin(),candidates.end(), v), candidates.end());
+				if(candidates.empty()){
 					state = SEARCHING_ADDITIONALLY;
 				}
 			}
 
 			if(state == SEARCHING_ADDITIONALLY){
+				maze.updateWall(v, w);
+				maze.updateStepMap({start});
+				candidates.clear();
+				for(int i=maze.getStep(v); i<maze.getStep(start); i--){
+					for(dir_t dir: {0+d, 1+d, 3+d, 2+d}){
+						Vector next = v.next(dir);
+						if(maze.getStep(next) == i-1 && maze.getWall(next).nDone()!=4){
+							candidates.push_back(next);
+						}
+					}
+				}
 				state = BACKING_TO_START;
 			}
 
@@ -351,13 +366,31 @@ class MazeAgent{
 	private:
 		State state;
 		Maze maze;
-		const Vector start = Vector(0, 0);
+		const Vector start{0, 0};
 		std::vector<Vector> goal;
 		Vector curVec;
 		dir_t curDir;
 		dir_t nextDir;
 
-		std::vector<Vector> list;
+		std::vector<Vector> candidates;
+		/*
+		   void calcNextDirMap(){
+		   step_t min_step = MAZE_STEP_MAX;
+		   for(dir_t dir: {0+d, 1+d, 3+d, 2+d}){
+		   dir &= 3;
+		   if(maze.getStep(v.next(dir))<min_step && !maze.getWall(v)[dir]){
+		   min_step = maze.getStep(v.next(dir));
+		   nextDir = dir;
+		   }
+		   }
+		   if(min_step == MAZE_STEP_MAX) state = GOT_LOST;
+		   if(std::any_of(goal.begin(), goal.end(), [&v](const auto &g){return g==v;})){
+		   state = REACHED_GOAL;
+		   candidates = goal;
+		   candidates.erase(std::find(candidates.begin(), candidates.end(), v));
+		   }
+		   }
+		   */
 };
 
 const char mazeData_fp2016[8+1][8+1] = {
@@ -391,29 +424,29 @@ extern const char mazeData_maze[16+1][16+1] = {
 };
 
 int main(void){
-	Maze sample(mazeData_fp2016);
-	//Maze sample(mazeData_maze,false);
-	//std::vector<Vector> goal = {Vector(7,7),Vector(7,8),Vector(8,8),Vector(8,7)};
-	std::vector<Vector> goal = {Vector(7,7)};
+	Maze sample(mazeData_maze,false);
+	std::vector<Vector> goal = {Vector(7,7),Vector(7,8),Vector(8,8),Vector(8,7)};
+	//Maze sample(mazeData_fp2016);
+	//std::vector<Vector> goal = {Vector(7,7)};
 	MazeAgent agent(goal);
 	setvbuf(stdout, (char *)NULL, _IONBF, 0);
 	agent.update(Vector(0, 0), 1, sample.getWall(0,0));
 	agent.printInfo(0);
-	sleep(1);
+	usleep(100000);
 	for(int step=1; ; step++){
-		if(agent.getState() == MazeAgent::REACHED_START){
-			printf("End\n");
-			break;
-		}
-		if(agent.getState() == MazeAgent::GOT_LOST){
-			printf("GOT LOST!\n");
-			break;
-		}
-		dir_t nextDir = agent.getNextDir();
-		Vector nextVec = agent.getCurVec().next(nextDir);
-		agent.update(nextVec, nextDir, sample.getWall(nextVec));
-		agent.printInfo(step);
-		sleep(1);
+	if(agent.getState() == MazeAgent::REACHED_START){
+	printf("End\n");
+	break;
+	}
+	if(agent.getState() == MazeAgent::GOT_LOST){
+	printf("GOT LOST!\n");
+	break;
+	}
+	dir_t nextDir = agent.getNextDir();
+	Vector nextVec = agent.getCurVec().next(nextDir);
+	agent.update(nextVec, nextDir, sample.getWall(nextVec));
+	agent.printInfo(step);
+	usleep(100000);
 	}
 	return 0;
 }
