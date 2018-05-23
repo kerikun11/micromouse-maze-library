16886540a06764528de13bdeeaa584c1b50228bf
kerikun11 <kerikun11+github@gmail.com>
2018-05-23 22:30:36 +0900

    ESP32の実行に必要なオプションを追加

--- cmake/toolchain_xtensa-esp32-elf.cmake
+++ cmake/toolchain_xtensa-esp32-elf.cmake
@@ -5,9 +5,9 @@ set(CMAKE_C_COMPILER xtensa-esp32-elf-gcc)
 set(CMAKE_CXX_COMPILER xtensa-esp32-elf-g++)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
-# set(CMAKE_FIND_ROOT_PATH /home/kerikun11/bin/xtensa-esp32-elf/xtensa-esp32-elf)
-# set(CMAKE_FIND_ROOT_PATH C:/msys64/opt/xtensa-esp32-elf/xtensa-esp32-elf)
+SET(CMAKE_C_FLAGS "-nostdlib -mlongcalls" CACHE STRING "" FORCE)
+SET(CMAKE_CXX_FLAGS "-nostdlib -mlongcalls" CACHE STRING "" FORCE)
+
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
