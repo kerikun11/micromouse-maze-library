56bab87e26ab663816c6ce9a25be2435f69fd71e
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-03-13 22:02:54 +0900

    add test

--- test_gtest/test_Maze.cpp
+++ test_gtest/test_Maze.cpp
@@ -3,7 +3,10 @@
 
 using namespace MazeLib;
 
-TEST(MazeTest, Position) {
-  const auto p = Position(0, 0);
-  EXPECT_EQ(p, p);
+TEST(Maze, parse) {
+  /* ファイルから迷路情報を取得 */
+  Maze maze;
+  const std::string file_path = "../mazedata/32MM2019HX.maze";
+  EXPECT_TRUE(maze.parse(file_path.c_str()));
+  EXPECT_TRUE(maze.canGo(Position(0, 0), Direction::North));
 }
