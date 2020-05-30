bdf5432a3beda38ac0dd03f9d66b82b64a78e393
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-05-30 12:09:42 +0900

    update README

--- search/functions_2.js
+++ search/functions_2.js
@@ -4,5 +4,5 @@ var searchData=
   ['calcshortestdirections_125',['calcShortestDirections',['../classMazeLib_1_1StepMap.html#a21f0f720ac4616d2dcac5f7d40f77eaf',1,'MazeLib::StepMap::calcShortestDirections(const Maze &amp;maze, const Position &amp;start, const Positions &amp;dest, const bool known_only, const bool simple)'],['../classMazeLib_1_1StepMap.html#a17c796aad41df5e65429c00fda794e7a',1,'MazeLib::StepMap::calcShortestDirections(const Maze &amp;maze, const bool known_only, const bool simple)']]],
   ['calcstraightsteptable_126',['calcStraightStepTable',['../classMazeLib_1_1StepMap.html#aa2f24ebd8f9cc5044132e07008e5c0a0',1,'MazeLib::StepMap']]],
   ['cango_127',['canGo',['../classMazeLib_1_1Maze.html#a9c14eb8cc30cc85bac3ebe7837dfb4b7',1,'MazeLib::Maze']]],
-  ['ceil_128',['ceil',['../namespaceMazeLib.html#ac1213d4846dc2d51c1ef990ac4a622be',1,'MazeLib']]]
+  ['ceil_128',['ceil',['../namespaceMazeLib.html#a1c35322019cb394d572442cfb4935aee',1,'MazeLib']]]
 ];
