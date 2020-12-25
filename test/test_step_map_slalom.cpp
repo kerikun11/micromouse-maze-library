69ec2e37b5638922f4e744782d32e8bd1929b448
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-12-25 11:47:40 +0900

    add test

---
+++ test/test_step_map_slalom.cpp
@@ -0,0 +1,17 @@
+#include "StepMapSlalom.h"
+#include "gtest/gtest.h"
+
+using namespace MazeLib;
+
+TEST(StepMapSlalom, operator_left_shift_left_shift) {
+  {
+    std::stringstream ss;
+    ss << StepMapSlalom::Index(1, 2, Direction::East);
+    EXPECT_EQ(ss.str(), "(  1,  2, >)");
+  }
+  {
+    std::stringstream ss;
+    ss << StepMapSlalom::Index(1, 2, 0, Direction::NorthEast);
+    EXPECT_EQ(ss.str(), "(  1,  2, >, ')");
+  }
+}
